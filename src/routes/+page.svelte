333568beb6f0f98e56079e8df4c48669f29e43fd
davis9001 <monaghan.david@gmail.com>
2024-04-18 11:28:29 -0700

    Init starspace

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to StarSpace</h1>
+<p>
+  Visit <a href="https://discord.gg/xsQC6URzyQ">https://discord.gg/xsQC6URzyQ</a
+  > to join the digital coworking revolution
+</p>
+<p>
+  *Space is an inclusive coworking space where everyone is welcome to work and
+  meet with others who are interested in getting things done.
+</p>
