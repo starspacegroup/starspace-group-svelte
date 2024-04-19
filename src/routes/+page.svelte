fb1b1a6515ad89b6c343f47cd9d8cc72776fcec3
davis9001 <monaghan.david@gmail.com>
2024-04-19 16:56:58 -0700

    Add flowbite and darkmode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,24 @@
-<h1>Welcome to StarSpace</h1>
-<p>
-  Visit <a href="https://discord.gg/xsQC6URzyQ">https://discord.gg/xsQC6URzyQ</a
-  > to join the digital coworking revolution
-</p>
-<p>
-  *Space is an inclusive coworking space where everyone is welcome to work and
-  meet with others who are interested in getting things done.
-</p>
+<script>
+  import { DarkMode } from "flowbite-svelte"
+</script>
+
+<main class="min-h-svh text-center dark:bg-black dark:text-white">
+  <nav class="p-2 text-right">
+    <DarkMode
+      class="text-primary-500 dark:text-primary-600 border dark:border-gray-800"
+    />
+  </nav>
+
+  <div class="p-8 text-2xl space-y-4 max-w-4xl mx-auto grid">
+    <h1 class="text-5xl mb-12">Welcome to *Space</h1>
+    <p>
+      Visit <a href="https://discord.gg/xsQC6URzyQ"
+        >https://discord.gg/xsQC6URzyQ</a
+      > to join the digital coworking revolution!
+    </p>
+    <p>
+      *Space is an inclusive coworking space where everyone is welcome to work
+      and meet with others who are interested in getting things done.
+    </p>
+  </div>
+</main>
