bed0bcf265c2075a66483d4f3c51e37b738b475b
davis9001 <monaghan.david@gmail.com>
2024-04-20 10:33:25 -0700

    Move content to variables and style improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,11 @@
   import { GithubSolid } from "flowbite-svelte-icons"
 
   let StarspaceLogoBackground = "/ai starspace logo transparent bg.png"
+  let SiteTitle = "*Space"
+  let SiteDescription =
+    "*Space (StarSpace) is an inclusive digital coworking space (currently on Discord) where everyone is welcome to work and meet with others who are interested in getting things done."
+  let GithubURL = "https://github.com/StarspaceGroup/"
+  let DiscordInviteURL = "https://discord.gg/xsQC6URzyQ"
 </script>
 
 <main
@@ -16,28 +21,26 @@
       <DarkMode
         class="text-primary-500 dark:text-primary-600 border dark:border-gray-800 bg-zinc-950 dark:bg-zinc-200 text-xl"
       />
-      <a
-        href="https://github.com/StarspaceGroup/starspace-group-svelte"
-        target="_blank"
-        class=""
-      >
+      <a href={GithubURL} target="_blank" class="">
         <GithubSolid size="xl" />
       </a>
     </nav>
     <div class="p-8 text-2xl space-y-4 max-w-4xl mx-auto grid">
-      <h1 class="text-5xl mb-12">Welcome to *Space</h1>
+      <h1
+        class="text-7xl md:text-9xl font-bold mb-12 text-zinc-100 hover:text-zinc-800 dark:text-zinc-800 dark:hover:text-zinc-100 duration-1000"
+      >
+        {SiteTitle}
+      </h1>
       <p>
         <Button
-          href="https://discord.gg/xsQC6URzyQ"
+          href={DiscordInviteURL}
           color="dark"
           class="text-2xl duration-1000 bg-[#7289DA] dark:bg-[#7289DA]"
           target="_blank">Join on Discord</Button
         >
       </p>
       <p>
-        *Space is an inclusive digital coworking space (currently on Discord)
-        where everyone is welcome to work and meet with others who are
-        interested in getting things done.
+        {SiteDescription}
       </p>
     </div>
   </div>
