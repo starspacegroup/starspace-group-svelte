09e42c23587e5dd55c4cd888235c7157c9a98ba4
davis9001 <monaghan.david@gmail.com>
2024-04-19 17:41:02 -0700

    Lotsa style changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,38 @@
 <script>
   import { DarkMode } from "flowbite-svelte"
+  import { Button } from "flowbite-svelte"
+  let StarspaceLogoBackground = "/ai starspace logo transparent bg.png"
 </script>
 
-<main class="min-h-svh text-center dark:bg-black dark:text-white">
-  <nav class="p-2 text-right">
-    <DarkMode
-      class="text-primary-500 dark:text-primary-600 border dark:border-gray-800"
-    />
-  </nav>
-
-  <div class="p-8 text-2xl space-y-4 max-w-4xl mx-auto grid">
-    <h1 class="text-5xl mb-12">Welcome to *Space</h1>
-    <p>
-      Visit <a href="https://discord.gg/xsQC6URzyQ"
-        >https://discord.gg/xsQC6URzyQ</a
-      > to join the digital coworking revolution!
-    </p>
-    <p>
-      *Space is an inclusive coworking space where everyone is welcome to work
-      and meet with others who are interested in getting things done.
-    </p>
+<main
+  class="min-h-svh text-center dark:bg-zinc-950 dark:text-zinc-200 bg-zinc-300 relative overflow-hidden"
+>
+  <div class="z-20 relative">
+    <nav class="p-2 text-right">
+      <DarkMode
+        class="text-primary-500 dark:text-primary-600 border dark:border-gray-800 bg-zinc-950 dark:bg-zinc-200"
+      />
+    </nav>
+    <div class="p-8 text-2xl space-y-4 max-w-4xl mx-auto grid">
+      <h1 class="text-5xl mb-12">Welcome to *Space</h1>
+      <p>
+        <Button
+          href="https://discord.gg/xsQC6URzyQ"
+          color="dark"
+          class="text-2xl duration-1000 bg-[#7289DA] dark:bg-[#7289DA]"
+          target="_blank">Join on Discord</Button
+        >
+      </p>
+      <p>
+        *Space is an inclusive digital coworking space (currently on Discord)
+        where everyone is welcome to work and meet with others who are
+        interested in getting things done.
+      </p>
+    </div>
   </div>
+  <img
+    src={StarspaceLogoBackground}
+    alt=""
+    class="absolute -left-1/3 -top-1/3 z-10 blur-[9rem]"
+  />
 </main>
