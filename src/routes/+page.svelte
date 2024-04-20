7b16cdb467118d9d24db6f1cdaba95b3375b7bf2
davis9001 <monaghan.david@gmail.com>
2024-04-19 21:17:05 -0700

    add github link in nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
   import { DarkMode } from "flowbite-svelte"
   import { Button } from "flowbite-svelte"
+  import { GithubSolid } from "flowbite-svelte-icons"
+
   let StarspaceLogoBackground = "/ai starspace logo transparent bg.png"
 </script>
 
@@ -8,10 +10,19 @@
   class="min-h-svh text-center dark:bg-zinc-950 dark:text-zinc-200 bg-zinc-300 relative overflow-hidden flex h-screen"
 >
   <div class="z-20 relative m-auto">
-    <nav class="p-5 text-right fixed w-full top-0 left-0">
+    <nav
+      class="p-5 text-right fixed w-full top-0 left-0 flex items-center justify-end space-x-6"
+    >
       <DarkMode
-        class="text-primary-500 dark:text-primary-600 border dark:border-gray-800 bg-zinc-950 dark:bg-zinc-200"
+        class="text-primary-500 dark:text-primary-600 border dark:border-gray-800 bg-zinc-950 dark:bg-zinc-200 text-xl"
       />
+      <a
+        href="https://github.com/StarspaceGroup/starspace-group-svelte"
+        target="_blank"
+        class=""
+      >
+        <GithubSolid size="xl" />
+      </a>
     </nav>
     <div class="p-8 text-2xl space-y-4 max-w-4xl mx-auto grid">
       <h1 class="text-5xl mb-12">Welcome to *Space</h1>
