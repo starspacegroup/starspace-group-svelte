c9e0ddbf92e6192c55990d2e49fccd4f330b34df
davis9001 <monaghan.david@gmail.com>
2024-04-26 17:47:40 -0700

    Use mock data in dev

--- src/MemberCount.svelte
+++ src/MemberCount.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte"
   import { Spinner } from "flowbite-svelte"
+  import { dev } from "$app/environment"
 
   let memberCount = {}
   let loading = true
@@ -8,12 +9,17 @@
 
   async function fetchMemberCount() {
     try {
-      const response = await fetch(
-        "https://discord.com/api/invites/xsQC6URzyQ?with_counts=true"
-      )
-      const data = await response.json()
-      memberCount = data.approximate_member_count // Change 'approximate_member_count' based on your guild structure
-      loading = false
+      if (dev) {
+        memberCount = 9999
+        loading = false
+      } else {
+        const response = await fetch(
+          "https://discord.com/api/invites/xsQC6URzyQ?with_counts=true"
+        )
+        const data = await response.json()
+        memberCount = data.approximate_member_count // Change 'approximate_member_count' based on your guild structure
+        loading = false
+      }
     } catch (error) {
       console.error("Error fetching member count:", error)
       errorMessage = "Failed to fetch member count"
