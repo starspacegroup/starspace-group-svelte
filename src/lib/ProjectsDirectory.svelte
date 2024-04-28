5c497f201162495edee9ae5376a2f55469920d1d
davis9001 <monaghan.david@gmail.com>
2024-04-28 01:13:41 -0700

    Add badges

--- src/lib/ProjectsDirectory.svelte
+++ src/lib/ProjectsDirectory.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
-  import { Button, Card, DescriptionList } from "flowbite-svelte"
+  import { Button, Card } from "flowbite-svelte"
   import { ArrowUpRightFromSquareOutline } from "flowbite-svelte-icons"
+  import { Badge } from "flowbite-svelte"
 
   const projects = [
-    {
-      name: "Physics and Audio",
-      description:
-        "An experiment in using the Web Audio API to play sounds in response to 2d physics objects interactions.",
-      url: "/projects/physics-and-audio",
-    },
+    // {
+    //   name: "Physics and Audio",
+    //   description:
+    //     "An experiment in using the Web Audio API to play sounds in response to 2d physics objects interactions.",
+    //   url: "/projects/physics-and-audio",
+    // },
     {
       name: "Reddisco.win",
       description:
         "A web utility to make sharing Discord invite links on Reddit faster. Takes an invite link as input, fetches the description of the server from Discord as the post title, then offers edits to the title using AI. The provided link and title are auto filled by clicking the button(s) for a handful of subreddit(s).",
       url: "https://reddisco.win",
       external: true,
+      tags: [
+        { title: "Made at *Space", color: "blue" },
+        { title: "Open Source", color: "green" },
+      ],
     },
     {
       name: "Spacetime Clock",
@@ -22,6 +27,11 @@
         "A date and time clock where the current day and the current time are represented as a degree of a circle (0-359) with the start date being the last summer solstice and the start time being solar noon.",
       url: "https://spacetime-clock.pages.dev/",
       external: true,
+      tags: [
+        { title: "Solar Hackathon 2024", color: "yellow" },
+        { title: "Made at *Space", color: "blue" },
+        { title: "Open Source", color: "green" },
+      ],
     },
   ]
 </script>
@@ -36,15 +46,22 @@
           <a href={project.url} target="_blank">{project.name}</a>
         </h5>
         <p
-          class="mb-12 font-light text-gray-700 dark:text-gray-300 leading-tight text-left"
+          class="mb-5 font-light text-gray-700 dark:text-gray-300 leading-tight text-left"
         >
           {project.description}
         </p>
-        <div class="text-right absolute bottom-3">
+        {#if project.tags}
+          <div class="flex flex-wrap gap-2 pb-24">
+            {#each project.tags as tag}
+              <Badge color={tag.color} class="mr-2 text-lg">{tag.title}</Badge>
+            {/each}
+          </div>
+        {/if}
+        <div class="text-right absolute bottom-3 right-3">
           <Button
             href={project.url}
             target={project.external ? "_blank" : ""}
-            class="gap-2"
+            class="gap-2 text-xl"
             >Check it Out! {#if project.external}
               <ArrowUpRightFromSquareOutline />
             {/if}
