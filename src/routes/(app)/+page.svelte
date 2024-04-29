d6c84b66b8736afce781ec7c693ff9dc73c112fc
davis9001 <monaghan.david@gmail.com>
2024-04-29 14:01:27 -0700

    Add meta title and description

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,6 +2,14 @@
   import DiscordServerInfo from "$lib/DiscordServerInfo.svelte"
 </script>
 
+<svelte:head>
+  <title>*Space - Coworking on Discord</title>
+  <meta
+    name="description"
+    content="Work around and with rock star developers, artists, and more!"
+  />
+</svelte:head>
+
 <div>
   <DiscordServerInfo />
 </div>
--- src/routes/(app)/projects/+page.svelte
+++ src/routes/(app)/projects/+page.svelte
@@ -2,6 +2,14 @@
   import ProjectsDirectory from "$lib/ProjectsDirectory.svelte"
 </script>
 
+<svelte:head>
+  <title>*Space - Projects</title>
+  <meta
+    name="description"
+    content="Check out some of the projects made by people working in *Space."
+  />
+</svelte:head>
+
 <div>
   <ProjectsDirectory />
 </div>
