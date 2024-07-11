e0e9fb45153d3ea290640e5ceb773db153993ea3
davis9001 <monaghan.david@gmail.com>
2024-07-11 04:21:39 -0700

    Add github link

--- src/routes/discord-bot/+page.svelte
+++ src/routes/discord-bot/+page.svelte
@@ -15,12 +15,17 @@
   ]
   import { DarkMode } from "flowbite-svelte"
   import "../../app.pcss"
+  import { GithubSolid } from "flowbite-svelte-icons"
+  const GithubURL = "https://github.com/StarspaceGroup/starspace-bot"
 </script>
 
 <div class="p-14 dark:bg-zinc-950 dark:text-zinc-200 bg-zinc-300 min-h-svh">
   <DarkMode
     class="text-primary-500 dark:text-primary-600 border dark:border-gray-800 bg-zinc-950 dark:bg-zinc-200 text-xl absolute top-12 right-12"
   />
+  <a href={GithubURL} target="_blank" class="absolute bottom-12 right-12">
+    <GithubSolid size="xl" />
+  </a>
 
   <h1 class="text-4xl md:text-9xl">*Space Discord Bot</h1>
   <p class="m-9 text-3xl">
