152c2d987ded33657692249e35935af0b0c4b864
davis9001 <monaghan.david@gmail.com>
2024-12-20 23:53:31 -0700

    Add Bluesky link

--- src/lib/SocialIcons.svelte
+++ src/lib/SocialIcons.svelte
@@ -1,16 +1,21 @@
 <script>
   import { GithubSolid, InstagramSolid, XSolid } from "flowbite-svelte-icons"
+  import BlueskySolid from "./BlueskySolid.svelte";
   const GithubURL = "https://github.com/StarspaceGroup/"
   const InstagramURL = "https://www.instagram.com/starspace.group/"
   const XURL = "https://x.com/starspacegroup/"
+  const BlueskyURL = "https://bsky.app/profile/starspace.group"
 </script>
 
-<a href={GithubURL} target="_blank" class="">
+<a href={GithubURL} target="_blank">
   <GithubSolid size="xl" />
 </a>
-<a href={InstagramURL} target="_blank" class="">
+<a href={InstagramURL} target="_blank">
   <InstagramSolid size="xl" />
 </a>
-<a href={XURL} target="_blank" class="">
+<a href={XURL} target="_blank">
   <XSolid size="xl" />
+</a>
+<a href={BlueskyURL} target="_blank">
+  <BlueskySolid />
 </a>
