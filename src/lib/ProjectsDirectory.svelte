c3ae22e91eccfa66f8b75d944306ebc6a1017ce7
davis9001 <monaghan.david@gmail.com>
2024-12-21 06:11:16 -0700

    Add AgapeVerse to /projects

--- src/lib/ProjectsDirectory.svelte
+++ src/lib/ProjectsDirectory.svelte
@@ -2,6 +2,7 @@
   import { Button, Card } from "flowbite-svelte"
   import { ArrowUpRightFromSquareOutline } from "flowbite-svelte-icons"
   import { Badge } from "flowbite-svelte"
+  import { get } from "svelte/store"
 
   const projects = [
     // {
@@ -11,16 +12,19 @@
     //   url: "/projects/physics-and-audio",
     // },
     {
-      name: "MetaDock",
+      name: "AgapeVerse.app",
       description:
-        "A split screen browser with app support for multitasking. Available on Windows and soon Mac OS.",
-      url: "https://www.metadock.net/",
+        `An AI love poem generator, create and share your affection free! Advanced features include adding a "hidden" message that is spelled out in the first letters of each line of the poem.`,
+      url: "https://agapeverse.app/",
       external: true,
-      logo: "/metadock-logo.webp",
-      screenshot: "/metadock-screenshot.webp",
+      logo: "/agapeverse-logo.png",
+      screenshot: "/agapeverse-screenshot.jpg",
       tags: [
-        { title: "C++", color: "green" },
-        { title: "QT", color: "green" },
+        { title: "Made at *Space", color: "red" },
+        { title: "Deno Fresh", color: "green" },
+        { title: "Deno Deploy", color: "green" },
+        { title: "Cloudflare AI Gateway", color: "green" },
+        { title: "Open AI", color: "green" },
       ],
     },
     {
@@ -28,9 +32,10 @@
       description:
         "A web utility to make sharing Discord invite links on Reddit faster. Takes an invite link as input, fetches the description of the server from Discord as the post title, then offers edits to the title using AI. The provided link and title are auto filled by clicking the subreddit buttons.",
       url: "https://reddisco.win",
+      screenshot: '/reddisco-screenshot.jpg',
       external: true,
       tags: [
-        { title: "Made at *Space", color: "orange" },
+        { title: "Made at *Space", color: "red" },
         { title: "Sveltekit", color: "green" },
         { title: "Cloudflare Pages", color: "green" },
         { title: "Cloudflare Workers", color: "green" },
@@ -42,12 +47,26 @@
       description:
         "A web date and time clock where the current day and the current time are represented as a degree of a circle (0-359) with the start date being the last summer solstice and the start time being solar noon.",
       url: "https://spacetime-clock.pages.dev/",
+      screenshot: '/spacetime-clock-screenshot.jpg',
       external: true,
       tags: [
-        { title: "Solar Hackathon 2024", color: "yellow" },
+        { title: "Solar Hackathon 2024", color: "yellow"},
         { title: "Made at *Space", color: "red" },
         { title: "Sveltekit", color: "green" },
         { title: "Cloudflare Pages", color: "green" },
+      ],
+    },
+    {
+      name: "MetaDock",
+      description:
+        "A split screen browser with app support for multitasking. Available on Windows and soon Mac OS.",
+      url: "https://www.metadock.net/",
+      external: true,
+      logo: "/metadock-logo.webp",
+      screenshot: "/metadock-screenshot.webp",
+      tags: [
+        { title: "C++", color: "green" },
+        { title: "QT", color: "green" },
       ],
     },
   ]
