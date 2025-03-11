baf55ca962c749e215dbb96e9637f1505417e6b6
davis9001 <monaghan.david@gmail.com>
2025-03-11 06:20:02 -0400

    Comment out link to X (for now at least)

--- src/lib/SocialIcons.svelte
+++ src/lib/SocialIcons.svelte
@@ -3,7 +3,7 @@
   import BlueskySolid from "./BlueskySolid.svelte";
   const GithubURL = "https://github.com/StarspaceGroup/"
   const InstagramURL = "https://www.instagram.com/starspace.group/"
-  const XURL = "https://x.com/starspacegroup/"
+  // const XURL = "https://x.com/starspacegroup/"
   const BlueskyURL = "https://bsky.app/profile/starspace.group"
 </script>
 
@@ -13,9 +13,9 @@
 <a href={InstagramURL} target="_blank">
   <InstagramSolid size="xl" />
 </a>
-<a href={XURL} target="_blank">
+<!-- <a href={XURL} target="_blank">
   <XSolid size="xl" />
-</a>
+</a> -->
 <a href={BlueskyURL} target="_blank">
   <BlueskySolid />
 </a>
